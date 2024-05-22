cf78529de6f19affcdbc82034486b143b2dc03ff
Juan Garcia <juangarcia123457854@gmail.com>
2024-05-22 12:11:19 -0500

    Patio

---
+++ js/patio/create.js
@@ -0,0 +1,37 @@
+document.getElementById('createOrdenCargue').addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    const formData = new FormData(this);
+  
+    const jsonData = JSON.stringify(Object.fromEntries(formData));
+
+    console.log(jsonData)
+    
+    fetch('http://esenttiapp.test/api/ordenCargue', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: jsonData
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Error al enviar los datos del formulario');
+        }
+    })
+    .then(data => {
+        Swal.fire({
+          title: "¡Buen trabajo!",
+          text: "¡Has creado un Cliente",
+          icon: "success",
+        });
+    })
+    .then((response)=>{
+     time();
+    })
+  });
+
+function time() {
+    document.getElementById('createOrdenCargue').reset();
+    setTimeout(() => {
+        window.location.href = ``; 
+    },  1200);
+}
