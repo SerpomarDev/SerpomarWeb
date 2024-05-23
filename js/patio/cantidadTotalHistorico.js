863a70dd37df6ef7044d419e08e980ca71acdbe7
SerpomarDev-Luifer <luifer.villa@serpomar.com>
2024-05-23 17:05:52 -0500

    Historico contenedores

---
+++ js/patio/cantidadTotalHistorico.js
@@ -0,0 +1,15 @@
+fetch('https://esenttiapp-production.up.railway.app/api/historicototalcontenedor')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Error al obtener los datos de la API');
+    }
+    return response.json();
+  })
+  .then(data => {
+    const totalRegistrosElement = document.getElementById('historicoTotal');
+    totalRegistrosElement.textContent = `${data}`;
+    console.log(totalRegistrosElement)
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
