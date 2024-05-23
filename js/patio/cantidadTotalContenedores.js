86e9bb21d84eb4f748679b6021d8f1240d2d2a79
SerpomarDev-Luifer <luifer.villa@serpomar.com>
2024-05-23 16:03:45 -0500

    fix

--- js/patio/cantidadTotalContenedores.js
+++ js/patio/cantidadTotalContenedores.js
@@ -7,7 +7,8 @@ fetch('https://esenttiapp-production.up.railway.app/api/cantidadtotalcontenedor'
   })
   .then(data => {
     const totalRegistrosElement = document.getElementById('totalContenedores');
-    totalRegistrosElement.textContent = `Total registros: ${data}`;
+    totalRegistrosElement.textContent = `${data}`;
+    console.log(totalRegistrosElement)
   })
   .catch(error => {
     console.error('Error:', error);
