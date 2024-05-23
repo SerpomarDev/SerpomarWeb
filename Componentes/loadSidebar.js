858e755dfaae01617f53c40f44e77c120855892b
Juan Garcia <juangarcia123457854@gmail.com>
2024-05-23 12:44:47 -0500

    Update sidebar

--- Componentes/loadSidebar.js
+++ Componentes/loadSidebar.js
@@ -3,6 +3,7 @@ function loadSidebar() {
         .then(response => response.text())
         .then(data => {
             document.querySelector('#sidebar-container').innerHTML = data;
+            console.log('Sidebar loaded'); // Mensaje de depuración
 
             // Inicializar menús desplegables usando jQuery
             $(document).ready(function() {
@@ -12,6 +13,7 @@ function loadSidebar() {
                     var $subMenu = $this.next('ul');
                     $subMenu.hide();
                     $this.attr('aria-expanded', 'false');
+                    console.log('Submenu hidden for:', $this); // Mensaje de depuración
                 });
 
                 $('.has-arrow').click(function(e) {
@@ -20,6 +22,7 @@ function loadSidebar() {
                     var $subMenu = $this.next('ul');
                     $subMenu.slideToggle(300); // Aumentar el tiempo de animación para mayor claridad
                     $this.attr('aria-expanded', $this.attr('aria-expanded') === 'false' ? 'true' : 'false');
+                    console.log('Submenu toggled for:', $this); // Mensaje de depuración
                 });
             });
         })
