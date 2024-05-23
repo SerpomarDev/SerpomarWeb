70672f4375c83c87f9ea1cd0001d0ee77d168977
Juan Garcia <juangarcia123457854@gmail.com>
2024-05-23 12:42:13 -0500

    Update sidebar

--- Componentes/loadSidebar.js
+++ Componentes/loadSidebar.js
@@ -5,21 +5,21 @@ function loadSidebar() {
             document.querySelector('#sidebar-container').innerHTML = data;
 
             // Inicializar menús desplegables usando jQuery
-            $(document).ready(function(){
-                $('.has-arrow').click(function(e) {
-                    e.preventDefault();
-                    var $this = $(this);
-                    var $subMenu = $this.next('ul');
-                    $subMenu.slideToggle(0);
-                    $this.attr('aria-expanded', $this.attr('aria-expanded') === 'false' ? 'true' : 'false');
-                });
-
-                // Ensure all submenus are closed on page load
+            $(document).ready(function() {
+                // Asegurarse de que todos los submenús estén cerrados al cargar la página
                 $('.has-arrow').each(function() {
                     var $this = $(this);
                     var $subMenu = $this.next('ul');
                     $subMenu.hide();
                     $this.attr('aria-expanded', 'false');
+                });
+
+                $('.has-arrow').click(function(e) {
+                    e.preventDefault();
+                    var $this = $(this);
+                    var $subMenu = $this.next('ul');
+                    $subMenu.slideToggle(300); // Aumentar el tiempo de animación para mayor claridad
+                    $this.attr('aria-expanded', $this.attr('aria-expanded') === 'false' ? 'true' : 'false');
                 });
             });
         })
