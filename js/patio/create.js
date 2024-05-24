ea2f79f6d6592ad33719880d4624a1c3743e7fb6
SerpomarDev-Luifer <luifer.villa@serpomar.com>
2024-05-24 10:53:15 -0500

    Fix Generador QR

--- js/patio/create.js
+++ js/patio/create.js
@@ -5,8 +5,6 @@ document.getElementById('createOrdenCargue').addEventListener('submit', function
   
     const jsonData = JSON.stringify(Object.fromEntries(formData));
 
-    console.log(jsonData)
-    
     fetch('https://esenttiapp-production.up.railway.app/api/ordencargue', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -23,10 +21,15 @@ document.getElementById('createOrdenCargue').addEventListener('submit', function
             title: 'Formulario enviado',
             text: 'El formulario ha sido enviado con éxito.',
             confirmButtonText: 'OK'
+
+        }).then((result) => {
+            if (result.isConfirmed) {
+                generarQRCode();
+            }
         });
     })
     .then((response)=>{
-     time();
+
     })
   });
 
@@ -36,3 +39,106 @@ function time() {
         window.location.href = ``; 
     },1200);
 }
+
+
+
+function generarQRCode() {
+
+    let requiredFields = document.querySelectorAll('#createOrdenCargue [required]');
+    let allFieldsFilled = true;
+    requiredFields.forEach(function(field) {
+        if (!field.value) {
+            allFieldsFilled = false;
+        }
+    });
+
+    if (!allFieldsFilled) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Campos incompletos',
+            text: 'Por favor, complete todos los campos requeridos.'
+        });
+        return;
+    }
+
+    function getSelectedText(id) {
+        let select = document.getElementById(id);
+        return select.options[select.selectedIndex].text;
+    }
+
+    let fechaSolicitud = document.getElementById('fecha_solicitud').value;
+    let idPlaca = getSelectedText('id_placa');
+    let idCliente = getSelectedText('id_cliente');
+    let idConductor = getSelectedText('id_conductor');
+    let modalidad = document.getElementById('modalidad').value;
+    let idTipoOperacion = getSelectedText('id_tipo_operacion');
+    let idDestino1 = getSelectedText('id_sitio_inspeccion');
+    let idDestino2 = getSelectedText('id_sitio_inspeccion1');
+    let idDestino3 = getSelectedText('id_sitio_inspeccion2');
+    let idNaviera = getSelectedText('id_naviera');
+    let contenedor = document.getElementById('contenedor').value;
+    let idTipoContenedor = getSelectedText('id_tipo_contenedor');
+    let peso = document.getElementById('peso').value;
+    let funcionarioTransporte = document.getElementById('funcionario_transporte').value;
+    let precinto = document.getElementById('precinto').value;
+    let vencimientoCutofi = document.getElementById('vencimiento_cutoff').value;
+    let horaSoli = document.getElementById('hora_soli').value;
+    let fechaSoli = document.getElementById('fecha_soli').value;
+    let observaciones = document.getElementById('observaciones').value;
+
+    let qrData = `
+        Fecha de Solicitud: ${fechaSolicitud}
+        Placa: ${idPlaca}
+        ID Cliente: ${idCliente}
+        Conductor: ${idConductor}
+        Modalidad: ${modalidad}
+        Tipo de Servicio: ${idTipoOperacion}
+        Destino 1: ${idDestino1}
+        Destino 2: ${idDestino2}
+        Destino 3: ${idDestino3}
+        Naviera: ${idNaviera}
+        Contenedor: ${contenedor}
+        Tipo de Contenedor: ${idTipoContenedor}
+        Peso: ${peso}
+        Funcionario de Transporte: ${funcionarioTransporte}
+        Precinto: ${precinto}
+        Vencimiento Cut Off: ${vencimientoCutofi}
+        Hora de Solicitud: ${horaSoli}
+        Fecha de Solicitud: ${fechaSoli}
+        Observaciones: ${observaciones}
+    `;
+
+    let qr = qrcode(0, 'L');
+    qr.addData(qrData);
+    qr.make();
+    document.getElementById('qrcode').innerHTML = qr.createImgTag();
+    
+    $('#qrModal').modal('show');
+}
+
+$('#qrModal').on('hidden.bs.modal', function (e) {
+    document.getElementById('qrcode').innerHTML = '';
+});
+
+document.getElementById('downloadQR').addEventListener('click', function() {
+    let qrImg = document.querySelector('#qrcode img');
+    let qrCanvas = document.createElement('canvas');
+    let context = qrCanvas.getContext('2d');
+    let qrImgWidth = qrImg.width;
+    let qrImgHeight = qrImg.height;
+    let titleHeight = 40;
+    qrCanvas.width = qrImgWidth;
+    qrCanvas.height = qrImgHeight + titleHeight;
+
+    context.font = "20px Arial";
+    context.textAlign = "center";
+    context.fillText("Orden de Cargue", qrCanvas.width / 2, 30);
+    context.drawImage(qrImg, 0, titleHeight, qrImgWidth, qrImgHeight);
+
+    let link = document.createElement('a');
+    link.href = qrCanvas.toDataURL("image/png");
+    link.download = 'qr_code.png';
+    link.click();
+    time();
+});
+
