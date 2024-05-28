b659522573577f5dc322e248961b401e402ae413
SerpomarDev-Luifer <luifer.villa@serpomar.com>
2024-05-28 10:55:20 -0500

    Seleccion de reportes

--- js/report/TotalContenedoresPatio.js
+++ js/report/TotalContenedoresPatio.js
@@ -1,7 +1,30 @@
-document.getElementById('ReporteTotalContenedores').addEventListener('click', function(event){
-    event.preventDefault();
+document.addEventListener('DOMContentLoaded',()=>{
 
-    fetch('https://esenttiapp-production.up.railway.app/api/exceltotalcontenedores')
+    const selector = document.getElementById('reporteSelector');
+
+    selector.addEventListener('change',function(event){
+
+        const selectedValue = selector.value;
+        let endpoint = '';
+
+        switch(selectedValue){
+            
+            case 'endpointone':
+                endpoint = 'https://esenttiapp-production.up.railway.app/api/exceltotalcontenedores';
+                break;
+
+            case 'endpointtwo':
+                endpoint = 'https://esenttiapp-production.up.railway.app/api/excelinventariocontenedores';
+                break;
+
+            default:
+                console.error('Opción no válida');
+                return;
+        }
+
+        selector.disabled = true;
+
+    fetch(endpoint)
     .then(response=>{
         if(!response.ok){
             throw new Error('Error al obtener los datos de la API');
@@ -30,8 +53,16 @@ document.getElementById('ReporteTotalContenedores').addEventListener('click', fu
         // Limpiar el objeto URL y remover el enlace del documento
         window.URL.revokeObjectURL(url);
         a.remove();
+
+         selector.value = "";
     })
     .catch((error)=>{
         console.error("Error", error)
     })
+    .finally(() => {
+        // Volver a habilitar el select
+        selector.disabled = false;
+    });
+
+  })
 })
