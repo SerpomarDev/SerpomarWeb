e78ddfbd984d6cb039f8710d1d14934b1c664d56
SerpomarDev-Luifer <luifer.villa@serpomar.com>
2024-05-30 22:28:26 -0500

    fix

--- js/contenedor/createContenedor.js
+++ js/contenedor/createContenedor.js
@@ -1,154 +1,6 @@
-// let queryString = window.location.search;
-// let urlParams = new URLSearchParams(queryString);
-// let id = urlParams.get("id");
-
-// cargarValores(id);
-
-// function cargarValores(id){
-//   fetch(`https://esenttiapp-production.up.railway.app/api/uploadsolisev/${id}`)
-//     .then((response) => {
-//       if (!response.ok) {
-//         throw new Error("Error al obtener los datos de la API");
-//       }
-//       return response.json();
-//     })
-//     .then((data) => {
-//       if(data.length > 0) {
-//         const ordneSev = data[0];
-//         document.getElementById("id_solicitud_servicio").value = ordneSev.id_primario;
-//         document.getElementById("do_pedido").value = ordneSev.do_pedido;
-//         document.getElementById("do_sp").value = ordneSev.do_sp;
-//         document.getElementById("contendedor").value = ordneSev.contendedor;
-//         document.getElementById("id_cliente").value = ordneSev.id_cliente;
-//         document.getElementById("cliente").value = ordneSev.cliente;
-//         document.getElementById("imp_exp").value = ordneSev.imp_exp;
-//         document.getElementById("eta").value = ordneSev.eta;
-//         document.getElementById("levante").value = ordneSev.levante;
-//         document.getElementById("documental").value = ordneSev.documental;
-//         document.getElementById("fisico").value = ordneSev.fisico;
-//         document.getElementById("libre_hasta").value = ordneSev.libre_hasta;
-//         document.getElementById("bodega_hasta").value = ordneSev.bodega_hasta;
-//         document.getElementById("propuesta").value = ordneSev.propuesta;
-
-//         // Actualizar el contenido del dashboard_bar
-//         document.querySelector(".dashboard_bar").textContent = ordneSev.do_sp;
-
-//         let id_cliente = ordneSev.id_cliente;
-//         tableByClt(id_cliente);
-
-//       } else {
-//         console.log('La propiedad array no existe en la respuesta');
-//       }
-//     })
-//     .catch((error) => {
-//       console.error('Error:', error);
-//     });
-// }
-
-// function tableByClt(id_cliente){
-//   new gridjs.Grid({
-//     search: true,
-//     language:{
-//       search:{
-//           placeholder: '🔍 Buscar...'
-//       }
-//     },
-//     pagination: {
-//         limit:5,
-//         enabled: false,
-//     },
-//     sort: false,
-//     columns: [{
-//       name:'id_co',
-//       hidden:true,
-//     },"SP","DO PEDIDO","Numero contenedor","Tipo transporte","Cliente",{
-//       name:'Acción',
-//       formatter:(cell,row)=>{
-//         return gridjs.h('button',{
-//           className: 'py-2 mb-4 px-4 border rounded bg-blue-600',
-//           onClick: () => asignar(row.cells[0].data)
-//         },'Asignar');
-//       }
-//     }],
-//     fixedHeader: true,
-//     server: {
-//       url: `https://esenttiapp-production.up.railway.app/api/preasigcont/${id_cliente}`,
-//       then: (data) => {
-//         if (Array.isArray(data) && data.length > 0) {
-//           return data.map((contenedorEx) => [
-//             contenedorEx.id,
-//             contenedorEx.do_sp,
-//             contenedorEx.do_pedido,
-//             contenedorEx.numero_co,
-//             contenedorEx.tipo_transporte,
-//             contenedorEx.cliente
-//           ]);
-//         } else {
-//           console.error("La respuesta del servidor no contiene datos válidos.");
-//           return [];
-//         }
-//       }
-//     },
-//     resizable: true,
-//     style: {
-//       table: {with:"80%"}
-//     }
-//   }).render(document.getElementById('contenedor'));
-// }
-
-// function asignar(id){
-//   let cliente  = id_cliente;
-
-//   if(cliente === 6){
-//     window.location.href = `/view/asignacion_esenttia/asignacion_esenttia.html?id=${id}`;
-//   }else{
-//     window.location.href = `/view/asignacion/asignacion.html?id=${id}`;
-//   }
-// }
-
-// document.getElementById('saveContenedor').addEventListener('submit',function(event){
-//   event.preventDefault();
-
-//   const formData = new FormData(this);
-//   const jsonData = JSON.stringify(Object.fromEntries(formData));
-
-//   fetch('https://esenttiapp-production.up.railway.app/api/contenedores',{
-//     method: 'POST',
-//     headers: { 'Content-Type': 'application/json' },
-//     body: jsonData
-//   })
-//   .then(response => {
-//     if (!response.ok) {
-//       throw new Error('Error al enviar los datos del formulario');
-//     }
-//   })
-//   .then(data => {
-//     Swal.fire({
-//       title: "¡Buen trabajo!",
-//       text: "¡Has creado un contenedor",
-//       icon: "success",
-//     });
-//   })
-//   .then((response)=>{
-//     time();
-//   })
-//   .catch((error) => {
-//     console.error("Error:", error);
-//   });
-// });
-
-// function time() {
-//   document.getElementById('saveContenedor').reset();
-//   setTimeout(() => {
-//     window.location.href = ``;
-//   },  1200);
-// }
-
 let queryString = window.location.search;
 let urlParams = new URLSearchParams(queryString);
 let id = urlParams.get("id");
-
-cargarValores(id)
 
 function actualizarEstado(idOperacion,nuevoEstado) {
   fetch(`https://esenttiapp-production.up.railway.app/api/actualizarestadocontenedor/${nuevoEstado}/${idOperacion}`, {
@@ -178,36 +30,40 @@ function actualizarEstado(idOperacion,nuevoEstado) {
   });
 }
 
-cargarValores(id)
+
+cargarValores(id);
 
 function cargarValores(id){
-fetch(`https://esenttiapp-production.up.railway.app/api/uploadsolisev/${id}`)
+  fetch(`https://esenttiapp-production.up.railway.app/api/uploadsolisev/${id}`)
     .then((response) => {
       if (!response.ok) {
-          throw new Error("Error al obtener los datos de la API");
+        throw new Error("Error al obtener los datos de la API");
       }
       return response.json();
     })
     .then((data) => {
       if(data.length > 0) {
         const ordneSev = data[0];
-          document.getElementById("id_solicitud_servicio").value = ordneSev.id_primario;
-          document.getElementById("do_pedido").value = ordneSev.do_pedido;
-          document.getElementById("do_sp").value = ordneSev.do_sp;
-          document.getElementById("contendedor").value = ordneSev.contendedor;
-          document.getElementById("id_cliente").value = ordneSev.id_cliente;
-          document.getElementById("cliente").value = ordneSev.cliente;
-          document.getElementById("imp_exp").value = ordneSev.imp_exp;
-          document.getElementById("eta").value = ordneSev.eta;
-          document.getElementById("levante").value = ordneSev.levante;
-          document.getElementById("documental").value = ordneSev.documental;
-          document.getElementById("fisico").value = ordneSev.fisico;
-          document.getElementById("libre_hasta").value = ordneSev.libre_hasta;
-          document.getElementById("bodega_hasta").value = ordneSev.bodega_hasta;
-          document.getElementById("propuesta").value = ordneSev.propuesta;
+        document.getElementById("id_solicitud_servicio").value = ordneSev.id_primario;
+        document.getElementById("do_pedido").value = ordneSev.do_pedido;
+        document.getElementById("do_sp").value = ordneSev.do_sp;
+        document.getElementById("contendedor").value = ordneSev.contendedor;
+        document.getElementById("id_cliente").value = ordneSev.id_cliente;
+        document.getElementById("cliente").value = ordneSev.cliente;
+        document.getElementById("imp_exp").value = ordneSev.imp_exp;
+        document.getElementById("eta").value = ordneSev.eta;
+        document.getElementById("levante").value = ordneSev.levante;
+        document.getElementById("documental").value = ordneSev.documental;
+        document.getElementById("fisico").value = ordneSev.fisico;
+        document.getElementById("libre_hasta").value = ordneSev.libre_hasta;
+        document.getElementById("bodega_hasta").value = ordneSev.bodega_hasta;
+        document.getElementById("propuesta").value = ordneSev.propuesta;
 
-          let id_cliente = ordneSev.id_cliente
-          tableByClt(id_cliente)
+        // Actualizar el contenido del dashboard_bar
+        document.querySelector(".dashboard_bar").textContent = ordneSev.do_sp;
+
+        let id_cliente = ordneSev.id_cliente;
+        tableByClt(id_cliente);
 
       } else {
         console.log('La propiedad array no existe en la respuesta');
@@ -216,11 +72,9 @@ fetch(`https://esenttiapp-production.up.railway.app/api/uploadsolisev/${id}`)
     .catch((error) => {
       console.error('Error:', error);
     });
-
 }
 
 function tableByClt(id_cliente){
-
   new gridjs.Grid({
     search: true,
     language:{
@@ -231,14 +85,13 @@ function tableByClt(id_cliente){
     // pagination: {
     //     limit:5,
     //     enabled: false,
-    //     //summary: true
     // },
     sort: false,
     columns: [{
       name:'id_co',
-      hidden: false,
+      hidden:true,
     },"SP","Numero contenedor","Fecha cita","Fecha Devolución","Estado operación",{
-    name:'Acción',
+      name:'Acción',
       formatter:(cell,row)=>{
         return gridjs.h('button',{
           className: 'py-2 mb-4 px-4 border rounded bg-blue-600 ',
@@ -247,7 +100,7 @@ function tableByClt(id_cliente){
           onClick: () => asignar(row.cells[0].data)
         },'Asignar')
       }
-    },{
+    } ,{
       name:"Pre-liquidar",
       formatter:(cell,row)=>{
         return gridjs.h('button',{
@@ -255,7 +108,7 @@ function tableByClt(id_cliente){
           onClick: () => preLiquidar(row.cells[0].data)
         },'ir')
       }
-    },{
+    }, {
       name:'Estado Operación',
       formatter: (cell, row) => {
         return gridjs.h('select', {
@@ -273,118 +126,83 @@ function tableByClt(id_cliente){
     },
     }],
     fixedHeader: true,
-    //height: '400px',
     server: {
-        url: `https://esenttiapp-production.up.railway.app/api/preasigcont/${id_cliente}`,
-        then: (data) => {
-            if (Array.isArray(data) && data.length > 0) {
-                return data.map((contenedorEx) => [
-                  contenedorEx.id,
-                  contenedorEx.do_sp,
-                  contenedorEx.numero_co,
-                  contenedorEx.fecha_cita,
-                  contenedorEx.fecha_devolucion,
-                  contenedorEx.estado_operacion,
-                ]);
-            } else {
-                console.error("La respuesta del servidor no contiene datos válidos.");
-                return [];
-            }
+      url: `https://esenttiapp-production.up.railway.app/api/preasigcont/${id_cliente}`,
+      then: (data) => {
+        if (Array.isArray(data) && data.length > 0) {
+          return data.map((contenedorEx) => [
+            contenedorEx.id,
+            contenedorEx.do_sp,
+            contenedorEx.numero_co,
+            contenedorEx.fecha_cita,
+            contenedorEx.fecha_devolucion,
+            contenedorEx.estado_operacion,
+          ]);
+        } else {
+          console.error("La respuesta del servidor no contiene datos válidos.");
+          return [];
         }
+      }
     },
     resizable: true,
     style: {
       table: {with:"80%"}
     }
-}).render(document.getElementById('contenedor'));
+  }).render(document.getElementById('contenedor'));
+
 
 function asignar(id){
-  
-  let cliente  = id_cliente
+  let cliente  = id_cliente;
 
-  // document.getElementById('id_contenedor').value = id; 
+  document.getElementById('id_contenedor').value = id; 
   $('#asignarModal').modal('show');
 
-  function cargarValores(id){
-
-    fetch(`https://esenttiapp-production.up.railway.app/api/asignacioncontenedors/${id}`)
-    .then((responde) => {
-      if (!responde) {
-        throw new Error("Error al obtener los datos de la API");
-      }
-      return responde.json();
-    })
-    .then((data) => {
-      if (data.length > 0) {
-        const asigcont = data[0];
-        document.getElementById("id_contenedor").value = asigcont.id_contenedor;
-        document.getElementById("id_cliente").value = asigcont.id_cliente;
-        document.getElementById("imp_exp").value = asigcont.imp_exp;
-    
-        let impExpValor = asigcont.imp_exp
-        let id_cliente  = asigcont.id_cliente
-        let id_contenedor = asigcont.id_contenedor
-    
-       crearTablas(id_contenedor,id_cliente,impExpValor)
-    
-      } else {
-        console.log("La propiedad array no existe en la respuesta");
-      }
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-    
-    }
   // if(cliente === 6){
-  //   window.location.href = `/view/asignacion_esenttia/asignacion_esenttia.html?id=${id}`
+  //   window.location.href = `/view/asignacion_esenttia/asignacion_esenttia.html?id=${id}`;
   // }else{
-  //   window.location.href = `/view/asignacion/asignacion.html?id=${id}`
+  //   window.location.href = `/view/asignacion/asignacion.html?id=${id}`;
   // }
- 
 }
 
 function preLiquidar(id){
   window.location.href = `/view/liquidar/pre_liquidar.html?id=${id}`
 }
+
 }
-
-
 document.getElementById('saveContenedor').addEventListener('submit',function(event){
-    event.preventDefault();
+  event.preventDefault();
 
-    const formData = new FormData(this);
-    const jsonData = JSON.stringify(Object.fromEntries(formData));
+  const formData = new FormData(this);
+  const jsonData = JSON.stringify(Object.fromEntries(formData));
 
-    fetch('https://esenttiapp-production.up.railway.app/api/contenedores',{
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: jsonData
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Error al enviar los datos del formulario');
-        }
-    })
-    .then(data => {
-        Swal.fire({
-          title: "¡Buen trabajo!",
-          text: "¡Has creado un contenedor",
-          icon: "success",
-        });
-    })
-    .then((response)=>{
-      time();
-    })
-    .catch((error) => {
-        console.error("Error:", error);
+  fetch('https://esenttiapp-production.up.railway.app/api/contenedores',{
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: jsonData
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Error al enviar los datos del formulario');
+    }
+  })
+  .then(data => {
+    Swal.fire({
+      title: "¡Buen trabajo!",
+      text: "¡Has creado un contenedor",
+      icon: "success",
     });
+  })
+  .then((response)=>{
+    time();
+  })
+  .catch((error) => {
+    console.error("Error:", error);
   });
+});
 
-
-  function time() {
-    document.getElementById('saveContenedor').reset();
-    setTimeout(() => {
-        window.location.href = ``; 
-    },  1200);
-  }  
+function time() {
+  document.getElementById('saveContenedor').reset();
+  setTimeout(() => {
+    window.location.href = ``;
+  },  1200);
+}
