54b7edd1c644319d2001884684a47697b7c0e0a5
SerpomarDev-Luifer <luifer.villa@serpomar.com>
2024-06-11 09:29:14 -0500

    fix remove pre-load modal fake

--- js/asignacion/asignacionModal.js
+++ js/asignacion/asignacionModal.js
@@ -1,33 +1,31 @@
 document.getElementById("saveAsignacion").addEventListener("submit", function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const formData = new FormData(this);
-    const jsonData = JSON.stringify(Object.fromEntries(formData));
+  const formData = new FormData(this);
+  const jsonData = JSON.stringify(Object.fromEntries(formData));
 
-    fetch("https://esenttiapp-production.up.railway.app/api/saveasignacion", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: jsonData,
+  fetch("https://esenttiapp-production.up.railway.app/api/saveasignacion", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: jsonData,
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Error al enviar los datos del formulario");
+      }
     })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Error al enviar los datos del formulario");
-        }
-      })
-      .then((data) => {
-        Swal.fire({
-          title: "¡Buen trabajo!",
-          text: " La asignacion se ha creado de forma exitosa.",
-          icon: "success"
-        });
-      })
-      .then((response) => {
-        time();
-      })
-      .catch((error) => {
-        console.error("Error:", error);
+    .then((data) => {
+      Swal.fire({
+        title: "¡Buen trabajo!",
+        text: " La asignacion se ha creado de forma exitosa.",
+        icon: "success"
       });
-  });
+      time();
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+});
 
 function time() {
   document.getElementById("saveAsignacion").reset();
