7861b3a7d2302bfcf0f677eb8915817602ea531a
SerpomarDev-Luifer <luifer.villa@serpomar.com>
2024-06-11 11:17:22 -0500

    add boton liquidar sp

--- js/Liquidar/pre_liquidar.js
+++ js/Liquidar/pre_liquidar.js
@@ -44,6 +44,8 @@ document.getElementById('savePreLiquidacion').addEventListener('submit',function
     const formData = new FormData(this);
     const jsonData = JSON.stringify(Object.fromEntries(formData));
 
+    console.log(jsonData)
+    
     fetch('https://esenttiapp-production.up.railway.app/api/costoclientecontenedor',{
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
