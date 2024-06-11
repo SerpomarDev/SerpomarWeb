ce495773170ffdf03d514a76abd4127660df9a21
SerpomarDev-Luifer <luifer.villa@serpomar.com>
2024-06-11 08:56:40 -0500

    fix new .js create contenedor

--- js/contenedor/createContenedor.js
+++ js/contenedor/createContenedor.js
@@ -188,37 +188,6 @@ function preLiquidar(id){
 }
 }
 
-document.getElementById('saveContenedor').addEventListener('submit',function(event){
-  event.preventDefault();
-
-  const formData = new FormData(this);
-  const jsonData = JSON.stringify(Object.fromEntries(formData));
-
-  fetch('https://esenttiapp-production.up.railway.app/api/contenedores',{
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: jsonData
-  })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Error al enviar los datos del formulario');
-    }
-  })
-  .then(data => {
-    Swal.fire({
-      title: "¡Buen trabajo!",
-      text: "¡Has creado un contenedor",
-      icon: "success",
-    });
-  })
-  .then((response)=>{
-    time();
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
-});
-
 function time() {
   document.getElementById('saveContenedor').reset();
   setTimeout(() => {
