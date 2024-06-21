1bdc4ff30a9d1bce5a36b10c9af0a8120e39646b
SerpomarDev-Luifer <luifer.villa@serpomar.com>
2024-06-21 17:32:33 -0500

    fixes

---
+++ js/contenedor/editContenedor.js
@@ -0,0 +1,69 @@
+let queryString = window.location.search;
+let urlParams = new URLSearchParams(queryString);
+let id = urlParams.get("id");
+
+fetch(`https://esenttiapp-production.up.railway.app/api/editcontenedor/${id}`)
+    .then((response) => {
+      if (!response.ok) {
+          throw new Error("Error al obtener los datos de la API");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      if (data.length > 0) {
+        const contenedor = data[0];
+          document.getElementById("id_contenedor").value = contenedor.id_contenedor
+          document.getElementById("tipo_contenedor").value = contenedor.tipo_contenedor
+          document.getElementById("nu_serie").value = contenedor.numero_contenedor
+          console.log(contenedor)
+
+      } else {
+        console.log('La propiedad array no existe en la respuesta');
+      }
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+
+
+    document.getElementById("editContenedor").addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const formData = new FormData(this);
+        const jsonData = JSON.stringify(Object.fromEntries(formData));
+
+        fetch(`https://esenttiapp-production.up.railway.app/api/contenedores/${id}`, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: jsonData,
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Error al enviar los datos del formulario");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                console.log("Respuesta del servidor:", data);
+                Swal.fire({
+                    title: "¡Buen trabajo!",
+                    text: "El contenedor ha sido actualizado.",
+                    icon: "success",
+                });
+            })
+            .then(response=>{
+                setTimeout(() => {
+                    history.back();
+                  }, 1500);
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    });
+
+    function time() {
+        document.getElementById('editContenedor').reset();
+        setTimeout(() => {
+          window.location.href = `/view/contenedor/edit.html`;  
+        },  1500);
+      }
--- js/placa/editPlaca.js
+++ js/placa/editPlaca.js
@@ -11,14 +11,13 @@ fetch(`https://esenttiapp-production.up.railway.app/api/editplaca/${id}`)
     })
     .then((data) => {
       if (data.length > 0) {
-        const conductor = data[0];
-          document.getElementById("id_placa").value = conductor.id_placa
-          document.getElementById("placa").value = conductor.placa
-          document.getElementById("eje").value = conductor.eje
-          document.getElementById("tipologia").value = conductor.tipologia
-          document.getElementById("id_aliado").value = conductor.id_aliado
-          document.getElementById("aliado").value = conductor.aliado
-          console.log(conductor)
+        const placa = data[0];
+          document.getElementById("id_placa").value = placa.id_placa
+          document.getElementById("placa").value = placa.placa
+          document.getElementById("eje").value = placa.eje
+          document.getElementById("tipologia").value = placa.tipologia
+          document.getElementById("id_aliado").value = placa.id_aliado
+          document.getElementById("aliado").value = placa.aliado
 
       } else {
         console.log('La propiedad array no existe en la respuesta');
