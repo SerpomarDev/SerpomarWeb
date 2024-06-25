495dd7f2fbac9a6ebcfe1d1c72782f01aa4baf54
Juan Garcia <juangarcia123457854@gmail.com>
2024-06-25 18:04:33 -0500

    VOLUMEN chart

--- js/graficas/volumenes.js
+++ js/graficas/volumenes.js
@@ -1,47 +1,42 @@
-// let ctx = document.getElementById('myChart').getContext('2d');
-// let options =  {
-//     type: 'bar',
-//     data: {
-//         labels: [],
-//         datasets: [{
-//             label: 'Valores',
-//             data: [],
-//             backgroundColor: [
-//                 'rgba(54, 162, 235, 0.2)',
-//                 'rgba(255, 206, 86, 0.2)'
-//             ],
-//             borderColor: [
-//                 'rgba(54, 162, 235, 1)',
-//                 'rgba(255, 206, 86, 1)'
-//             ],
-//             borderWidth: 1
-//         }]
-//     },
-//     options: {
-//         scales: {
-//             y: {
-//                 beginAtZero: true
-//             }
-//         }
-//     }
-// };
+// JS de Volumen (volumenes.js)
 
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Expo', 'Impo'],
+        datasets: [{
+            label: 'Valores',
+            data: [],
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)'
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
 
-
-let myChartse = new ApexCharts(ctx, options);
-myChartse.render();
-
-
-let urlse = "https://esenttiapp-production.up.railway.app/api/volumenesimpexp";
-fetch(urlse)
+fetch('https://esenttiapp-production.up.railway.app/api/volumenesimpexp')
     .then(response => response.json())
-    .then(itemsEs => viewEs(itemsEs))
-    .catch(error => console.log(error));
+    .then(data => {
+        const importacion = data.find(item => item.imp_exp === 'importacion').conteo;
+        const exportacion = data.find(item => item.imp_exp === 'exportacion').conteo;
 
-const viewEs = (estadoEs) => {
-    estadoEs.forEach(elementse => {
-        options.labels.push(elementse.estado);
-        options.series.push(elementse.conteo);
+        myChart.data.datasets[0].data = [exportacion, importacion];
+        myChart.update();
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
     });
-    myChartse.updateOptions(options);
-}
