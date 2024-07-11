46f0ab2abf9ad702b684fdf4dba30fd7cf43f4e1
Juan Garcia <juangarcia123457854@gmail.com>
2024-07-11 08:04:23 -0500

    Fix limpiar modal

--- js/asignacion/controlAsignaciones.js
+++ js/asignacion/controlAsignaciones.js
@@ -1,90 +1,79 @@
 function updateTotalAbiertas(data) {
-	const abiertas = data.filter(item => item[8] === 'ABIERTA'); // 8 es el índice de la columna "Estado"
-	document.getElementById('total-abiertas').textContent = abiertas.length;
-  
-	const totalTarifas = abiertas.reduce((sum, item) => sum + parseFloat(item[5]), 0); // 5 es el índice de la columna "Tarifa"
-	document.getElementById('valor-total-abiertas').textContent = `$ ${totalTarifas.toLocaleString()}`;
-  }
-  
-  new gridjs.Grid({
-	search: true,
-	language: {
-		search: {
-			placeholder: '🔍 Buscar...'
-		}
-	},
-	pagination: {
-		limit: 20,
-		enabled: false,
-	},
-	sort: false,
-	columns: [{name:"id",
-                hidden:false
-            },"Fecha", "SP", "Contenedor", "Placa", "Aliado", {
-			name: "Tarifa",
-			formatter: (_, row) => `$ ${(row.cells[6].data).toLocaleString()}`
-		}, "Ruta", "Nombre", "Estado", {
-			name: 'Soportes',
-			hidden: false,
-			formatter: (cell, row) => {
-				return gridjs.html(`
-					<button onclick="uploadId(${row.cells[0].data})" >Adjuntar Archivo</button>
-				`);
-			}
-		}, {
-			name: "enviar",
-			formatter: (cell, row) => {
-				return gridjs.h('button', {
-					className: 'py-2 mb-4 px-4 border rounded bg-blue-600',
-					onClick: () => editAsignacion()
-				}, 'enviar')
-			}
-		}
-	],
-	fixedHeader: true,
-	server: {
-		url: `https://esenttiapp-production.up.railway.app/api/controlasignaciones`,
-		then: (data) => {
-			if (Array.isArray(data) && data.length > 0) {
-				updateTotalAbiertas(data.map(asigControl => [
-					asigControl.id,
+    const abiertas = data.filter(item => item.estado === 'ABIERTA'); // Verifica que 'estado' sea 'ABIERTA'
+    document.getElementById('total-abiertas').textContent = abiertas.length;
+
+    const totalTarifas = abiertas.reduce((sum, item) => sum + parseFloat(item.tarifa || 0), 0); // Verifica que 'tarifa' sea un número válido
+    document.getElementById('valor-total-abiertas').textContent = `$ ${totalTarifas.toLocaleString()}`;
+}
+
+new gridjs.Grid({
+    search: true,
+    language: {
+        search: {
+            placeholder: '🔍 Buscar...'
+        }
+    },
+    pagination: {
+        limit: 20,
+        enabled: false,
+    },
+    sort: false,
+    columns: [
+        { name: "id", hidden: false },
+        "Fecha", "SP", "Contenedor", "Placa", "Aliado", {
+            name: "Tarifa",
+            formatter: (_, row) => `$ ${(row.cells[6].data).toLocaleString()}`
+        },
+        "Ruta", "Nombre", "Estado", {
+            name: 'Soportes',
+            hidden: false,
+            formatter: (cell, row) => {
+                return gridjs.html(`
+                    <button onclick="uploadId(${row.cells[0].data})">Adjuntar Archivo</button>
+                `);
+            }
+        }, {
+            name: "enviar",
+            formatter: (cell, row) => {
+                return gridjs.h('button', {
+                    className: 'py-2 mb-4 px-4 border rounded bg-blue-600',
+                    onClick: () => editAsignacion()
+                }, 'enviar')
+            }
+        }
+    ],
+    fixedHeader: true,
+    server: {
+        url: `https://esenttiapp-production.up.railway.app/api/controlasignaciones`,
+        then: (data) => {
+            if (Array.isArray(data) && data.length > 0) {
+                updateTotalAbiertas(data);
+                return data.map(asigControl => [
+                    asigControl.id,
                     asigControl.fecha,
-					asigControl.do_sp,
-					asigControl.numero_contenedor,
-					asigControl.placa,
-					asigControl.aliado,
-					asigControl.tarifa,
-					asigControl.ruta,
-					asigControl.nombre,
-					asigControl.estado,
-					asigControl.adjunto
-				]));
-				return data.map((asigControl) => [
-					asigControl.id,
-                    asigControl.fecha,
-					asigControl.do_sp,
-					asigControl.numero_contenedor,
-					asigControl.placa,
-					asigControl.aliado,
-					asigControl.tarifa,
-					asigControl.ruta,
-					asigControl.nombre,
-					asigControl.estado,
-					asigControl.adjunto,
-				]);
-			} else {
-				console.error("La respuesta del servidor no contiene datos válidos.");
-				return [];
-			}
-		}
-	},
-	resizable: true,
-	style: {
-		table: { width: "100%" }
-	}
-  }).render(document.getElementById('controlAsig'));
-  
-  document.addEventListener('DOMContentLoaded', (event) => {
+                    asigControl.do_sp,
+                    asigControl.numero_contenedor,
+                    asigControl.placa,
+                    asigControl.aliado,
+                    asigControl.tarifa,
+                    asigControl.ruta,
+                    asigControl.nombre,
+                    asigControl.estado,
+                    asigControl.adjunto
+                ]);
+            } else {
+                console.error("La respuesta del servidor no contiene datos válidos.");
+                return [];
+            }
+        }
+    },
+    resizable: true,
+    style: {
+        table: { width: "100%" }
+    }
+}).render(document.getElementById('controlAsig'));
+
+document.addEventListener('DOMContentLoaded', (event) => {
     const modal = document.getElementById('fileUploadModal');
     const span = document.getElementsByClassName('close')[0];
     let currentRowIndex;
@@ -106,13 +95,15 @@ function updateTotalAbiertas(data) {
 
     function uploadId(id) {
         document.getElementById('id_asignacion').value = id;
-        modal.style.display = 'block'; 
+        modal.style.display = 'block';
+
+        // RESETEAR EL MODAL
+        Dropzone.forElement("#SaveFile").removeAllFiles(true);
     }
 
     window.uploadId = uploadId;
 
     const form = document.getElementById('SaveFile');
-    //const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 
     const myDropzone = new Dropzone(form, {
         url: 'https://esenttiapp-production.up.railway.app/api/asignacionfile',
