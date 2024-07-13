0cc87897f0df98a6cf6cee0cd855fd5f709417fa
SerpomarDev-Luifer <luifer.villa@serpomar.com>
2024-07-13 14:18:18 -0500

    fixes costosAfiliados / ControlAsignacion

---
+++ js/contabilidad/costoAfiliado.js
@@ -0,0 +1,64 @@
+new gridjs.Grid({
+    search: true,
+    language: {
+        search: {
+            placeholder: '🔍 Buscar...'
+        }
+    },
+    pagination: {
+        limit: 20,
+        enabled: false,
+    },
+    sort: false,
+    columns: [
+        { name: "id", hidden: true },
+        "Fecha", "SP", "Contenedor", "Placa", "Aliado", {
+            name: "Tarifa",
+            formatter: (_, row) => `$ ${(row.cells[6].data).toLocaleString()}`
+        },
+        "Ruta", "Nombre", "Estado", {
+            name: 'Soportes',
+            hidden: true,
+            formatter: (cell, row) => {
+                return gridjs.html(`
+                    <button onclick="uploadId(${row.cells[0].data})">Adjuntar Archivo</button>
+                `);
+            }
+        }, {
+            name: "enviar",
+            formatter: (cell, row) => {
+                return gridjs.h('button', {
+                    className: 'py-2 mb-4 px-4 border rounded bg-blue-600',
+                    onClick: () => updateState(row.cells[0].data)
+                }, 'enviar')
+            }
+        }
+    ],
+    fixedHeader: true,
+    server: {
+        url: `https://esenttiapp-production.up.railway.app/api/asignacionespendientepago`,
+        then: (data) => {
+            if (Array.isArray(data) && data.length > 0) {
+                return data.map(asigControl => [
+                    asigControl.id,
+                    asigControl.fecha,
+                    asigControl.do_sp,
+                    asigControl.numero_contenedor,
+                    asigControl.placa,
+                    asigControl.aliado,
+                    asigControl.tarifa,
+                    asigControl.ruta,
+                    asigControl.nombre,
+                    asigControl.estado,
+                ]);
+            } else {
+                console.error("La respuesta del servidor no contiene datos válidos.");
+                return [];
+            }
+        }
+    },
+    resizable: true,
+    style: {
+        table: { width: "100%" }
+    }
+}).render(document.getElementById('costoAfiliado'));
