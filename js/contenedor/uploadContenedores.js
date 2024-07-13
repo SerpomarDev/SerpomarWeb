aaf451cb6a07122ad7ebfb845baf6eb50a2e834a
Juan Garcia <juangarcia123457854@gmail.com>
2024-07-13 14:15:14 -0500

    Upload document

--- js/contenedor/uploadContenedores.js
+++ js/contenedor/uploadContenedores.js
@@ -1,8 +1,8 @@
-// SDKs necesario
+// SDKs necesarios
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
 import { getStorage, ref, uploadBytes, getDownloadURL, listAll } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-storage.js";
 
-// configuracion firebase
+// Configuración Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyBQh7eqWl_iu02oDPds3nQMigzSrHDOFn0",
     authDomain: "serpomar-driver-mysql-f8df6.firebaseapp.com",
@@ -12,14 +12,29 @@ const firebaseConfig = {
     appId: "1:840427888757:web:b8250feec992a76510583c"
 };
 
-// inicio Firebase
+// Inicialización de Firebase
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
+
+// Función para obtener el ID de la solicitud desde la URL
+function getQueryParam(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+}
+
+// Genera un UUID
+function generateUUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        const r = (Math.random() * 16) | 0,
+            v = c === 'x' ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+    });
+}
 
 document.addEventListener('DOMContentLoaded', (event) => {
     const modal = document.getElementById('fileUploadModal');
     const span = document.getElementsByClassName('close')[0];
-    let currentRowIndex;
+    const idAsignacion = getQueryParam('id'); // Obtiene el ID de la solicitud desde la URL
 
     span.onclick = function () {
         modal.style.display = 'none';
@@ -32,12 +47,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
 
     document.getElementById('btnAdjuntos').onclick = function () {
-        const id = document.getElementById('id_asignacion').value;
-        uploadId(id);
+        uploadId(idAsignacion);
     }
 
     async function uploadId(id) {
-        document.getElementById('id_asignacion').value = id;
         modal.style.display = 'block';
         if (Dropzone.instances.length > 0) {
             Dropzone.instances.forEach(dz => dz.destroy());
@@ -47,22 +60,21 @@ document.addEventListener('DOMContentLoaded', (event) => {
             acceptedFiles: '.pdf,.doc,.docx,.xls,.xlsx,.txt,.jpg,.png,.jpeg',
             init: function () {
                 this.on('addedfile', async function (file) {
-                    const id = document.getElementById('id_asignacion').value;
                     let fileName = file.name;
-                    let filePath = `uploads/${id}/${fileName}`;
+                    let filePath = `spcontenedores/${id}/${fileName}`;
                     const storageRef = ref(storage, filePath);
 
-                    // Check if the file already exists
-                    const existingFiles = await listAll(ref(storage, `uploads/${id}`));
+                    // Verifica si el archivo ya existe
+                    const existingFiles = await listAll(ref(storage, `spcontenedores/${id}`));
                     const fileNames = existingFiles.items.map(item => item.name);
 
-                    // If the file name exists, append a timestamp
+                    // Si el nombre del archivo ya existe, añade un timestamp
                     if (fileNames.includes(fileName)) {
                         const timestamp = Date.now();
                         const fileExtension = fileName.substring(fileName.lastIndexOf('.'));
                         const fileNameWithoutExtension = fileName.substring(0, fileName.lastIndexOf('.'));
                         fileName = `${fileNameWithoutExtension}_${timestamp}${fileExtension}`;
-                        filePath = `uploads/${id}/${fileName}`;
+                        filePath = `spcontenedores/${id}/${fileName}`;
                     }
 
                     const newStorageRef = ref(storage, filePath);
@@ -87,7 +99,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
         fileList.innerHTML = '';
 
         try {
-            const listRef = ref(storage, `uploads/${id}`);
+            const listRef = ref(storage, `spcontenedores/${id}`);
             const res = await listAll(listRef);
 
             if (res.items.length === 0) {
@@ -95,22 +107,19 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 noFilesMessage.textContent = 'No hay archivos adjuntos.';
                 fileList.appendChild(noFilesMessage);
             } else {
-                res.items.forEach((itemRef) => {
-                    getDownloadURL(itemRef).then((url) => {
-                        const listItem = document.createElement('li');
-                        const link = document.createElement('a');
-                        link.href = url;
-                        link.textContent = itemRef.name;
-                        listItem.appendChild(link);
-                        fileList.appendChild(listItem);
-                    });
-                });
+                for (const itemRef of res.items) {
+                    const url = await getDownloadURL(itemRef);
+                    const listItem = document.createElement('li');
+                    const link = document.createElement('a');
+                    link.href = url;
+                    link.textContent = itemRef.name;
+                    listItem.appendChild(link);
+                    fileList.appendChild(listItem);
+                }
             }
         } catch (error) {
             console.error('Error loading uploaded files:', error);
             alert('Ocurrió un error al cargar los archivos adjuntos. Por favor, inténtelo de nuevo más tarde.');
         }
     }
-
-    window.uploadId = uploadId;
 });
