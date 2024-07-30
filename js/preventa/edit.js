3fcaae74ac3d0157b13752f21ba88c15f9734a27
SerpomarDev-Luifer <luifer.villa@serpomar.com>
2024-07-30 14:01:42 -0500

    edit preventa

--- js/preventa/edit.js
+++ js/preventa/edit.js
@@ -18,9 +18,9 @@ fetch(`https://esenttiapp-production.up.railway.app/api/cargaredit/${id}`)
         const preventa = data[0];
         document.getElementById("id").value = preventa.id;
           document.getElementById("id_placa").value = preventa.id_placa;
-          document.getElementById("placa").value = preventa.placa;
+          // document.getElementById("placa").value = preventa.placa;
           document.getElementById("id_conductor").value = preventa.id_conductor;
-          document.getElementById("conductor").value = preventa.conductor;
+          // document.getElementById("conductor").value = preventa.conductor;
           document.getElementById("identificacion").value = preventa.identificacion;
           document.getElementById("telefono").value = preventa.telefono;
           document.getElementById("eje").value = preventa.eje;
@@ -31,26 +31,6 @@ fetch(`https://esenttiapp-production.up.railway.app/api/cargaredit/${id}`)
           document.getElementById("cabot").value = preventa.cabot;
           document.getElementById("estado").value = preventa.estado;
           document.getElementById("flota").value = preventa.flota;
-
-          // Guarda los valores originales para su posterior comparación
-        originalValues = {
-          id_placa: preventa.id_placa,
-          placa: preventa.placa,
-          id_conductor: preventa.id_conductor,
-          conductor: preventa.conductor,
-          identificacion: preventa.identificacion,
-          telefono: preventa.telefono,
-          eje: preventa.eje,
-          tipologia: preventa.tipologia,
-          id_aliado: preventa.id_aliado,
-          aliado: preventa.aliado,
-          celular: preventa.celular,
-          proyecto: preventa.proyecto,
-          esenttia: preventa.esenttia,
-          cabot: preventa.cabot,
-          estado: preventa.estado,
-          flota: preventa.flota,
-        }
         
       } else {
         console.log('La propiedad array no existe en la respuesta');
@@ -64,42 +44,42 @@ fetch(`https://esenttiapp-production.up.railway.app/api/cargaredit/${id}`)
   
 document.getElementById("PreventaEdit").addEventListener("submit", function (event) {
         event.preventDefault();
-        // Compara los valores actuales con los originales
-        let currentValues = {
-            id_placa: document.getElementById("id_placa").value,
-            // placa: document.getElementById("placa").value,
-            id_conductor: document.getElementById("id_conductor").value,
-            // conductor: document.getElementById("conductor").value,
-            identificacion: document.getElementById("identificacion").value,
-            telefono: document.getElementById("telefono").value,
-            eje: document.getElementById("eje").value,
-            tipologia: document.getElementById("tipologia").value,
-            //id_aliado: document.getElementById("id_aliado").value,
-            //aliado: document.getElementById("aliado").value,
-            //celular: document.getElementById("celular").value,
-            proyecto: document.getElementById("proyecto").value,
-            esenttia: document.getElementById("esenttia").value,
-            cabot: document.getElementById("cabot").value,
-            estado: document.getElementById("estado").value,
-            flota: document.getElementById("flota").value,   
-        };
+    //     // Compara los valores actuales con los originales
+    //     let currentValues = {
+    //         id_placa: document.getElementById("id_placa").value,
+    //         // placa: document.getElementById("placa").value,
+    //         id_conductor: document.getElementById("id_conductor").value,
+    //         // conductor: document.getElementById("conductor").value,
+    //         identificacion: document.getElementById("identificacion").value,
+    //         telefono: document.getElementById("telefono").value,
+    //         eje: document.getElementById("eje").value,
+    //         tipologia: document.getElementById("tipologia").value,
+    //         //id_aliado: document.getElementById("id_aliado").value,
+    //         //aliado: document.getElementById("aliado").value,
+    //         //celular: document.getElementById("celular").value,
+    //         proyecto: document.getElementById("proyecto").value,
+    //         esenttia: document.getElementById("esenttia").value,
+    //         cabot: document.getElementById("cabot").value,
+    //         estado: document.getElementById("estado").value,
+    //         flota: document.getElementById("flota").value,   
+    //     };
 
-        // Verifica si hay cambios
-        let hasChanged = Object.keys(currentValues).some(
-            (key) => currentValues[key] !== originalValues[key]
-        );
+    //     // Verifica si hay cambios
+    //     let hasChanged = Object.keys(currentValues).some(
+    //         (key) => currentValues[key] !== originalValues[key]
+    //     );
 
-     /*    let hasChanged = JSON.stringify(originalValues) === JSON.stringify(currentValues)
-        console.log(hasChanged) */
+    //  /*    let hasChanged = JSON.stringify(originalValues) === JSON.stringify(currentValues)
+    //     console.log(hasChanged) */
 
-        if (!hasChanged) {
-            Swal.fire({
-                icon: "info",
-                title: "No hay cambios",
-                text: "No se han realizado cambios en el formulario.",
-            });
-            return; // Detiene la ejecución de la función
-        }
+    //     if (!hasChanged) {
+    //         Swal.fire({
+    //             icon: "info",
+    //             title: "No hay cambios",
+    //             text: "No se han realizado cambios en el formulario.",
+    //         });
+    //         return; // Detiene la ejecución de la función
+    //     }
 
         // Convertir formData a un objeto y luego a JSON
         const formData = new FormData(this);
