14524e0b0906e5592bf600ee98df54b2e1266132
SerpomarDev-Luifer <luifer.villa@serpomar.com>
2024-08-06 16:53:14 -0500

    fixes edit preventa

---
+++ js/cargar/editPCargarConductores.js
@@ -0,0 +1,61 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    //obtenemos los ids de los campos seleccionados
+    let selectNombre = document.getElementById('id_conductor');
+    let inputIdentificacion = document.getElementById('identificacion');
+    let inputTelefono = document.getElementById('telefono');
+  
+    new TomSelect('#id_conductor', {
+      valueField: 'id',
+      labelField: 'nombre',
+      searchField: 'nombre',
+      maxItems:1,
+      load: function(query, callback) {
+          fetch(`http://esenttiapp.test/api/uploadconductor?search=${encodeURIComponent(query)}`)
+              .then(response => response.json())
+              .then(data => {
+                  callback(data);
+              })
+              .catch(() => {
+                  callback();
+              });
+      },
+      render: {
+          option: function(item, escape) {
+              return `<div class="">
+                          ${escape(item.nombre)}
+                      </div>`;
+          },
+          item: function(item, escape) {
+              return `<div class="">
+                          ${escape(item.nombre)}
+                      </div>`;
+          }
+      }
+  });
+  
+      // capturamos los valores de telefono e identidicacion mediante el id seleccionado
+      selectNombre.addEventListener('change', function() {
+
+        let idConductorSeleccionado = this.value
+
+        fetch(`http://esenttiapp.test/api/uploadoptid/${idConductorSeleccionado}`)  
+          .then(response => {
+            if (!response.ok) {
+              throw new Error('Error en la respuesta de la API: ' + response.statusText);
+            }
+            return response.json();
+          })
+          .then(data => {
+            const conductor =  data[0]
+            if (conductor.identificacion && conductor.telefono) {
+                inputIdentificacion.value = conductor.identificacion;
+                inputTelefono.value = conductor.telefono;
+            } else {
+              console.error('Los datos esperados no están presentes en la respuesta de la API');
+            } 
+          })
+          .catch(error => console.error('Error:', error));
+      });
+  });
---
+++ js/cargar/editpCargarPlaca.js
@@ -0,0 +1,63 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+   let selectPlaca = document.getElementById('id_placa');
+    let inputeje = document.getElementById('eje');
+    let inputtipologia = document.getElementById('tipologia');
+    let inputid_aliado = document.getElementById('id_aliado');
+    let inputtelefono = document.getElementById('telefonoa');
+
+    new TomSelect('#id_placa', {
+        valueField: 'id',
+        labelField: 'placa',
+        searchField: 'placa',
+        maxItems:1,
+        load: function(query, callback) {
+            fetch(`http://esenttiapp.test/api/loadplaca?search=${encodeURIComponent(query)}`)
+                .then(response => response.json())
+                .then(data => {
+                    callback(data);
+                })
+                .catch(() => {
+                    callback();
+                });
+        },
+        render: {
+            option: function(item, escape) {
+                return `<div class="">
+                            ${escape(item.placa)}
+                        </div>`;
+            },
+            item: function(item, escape) {
+                return `<div class="">
+                            ${escape(item.placa)}
+                        </div>`;
+            }
+        }
+    });
+    
+
+    selectPlaca.addEventListener('change', function(){
+
+        let selectPlaca = this.value;
+    
+        fetch(`http://esenttiapp.test/api/uploadplacabyid/${selectPlaca}`)
+        .then(response =>{
+            if(!response.ok){
+                throw new Error('Error en la respuesta de la API: ' + response.statusText)
+            }
+            return response.json()
+        })
+        .then(data =>{
+            const placa = data[0]
+            if(placa.eje && placa.tipologia && placa.nombre && placa.celular){
+                inputeje.value = placa.eje;
+                inputtipologia.value = placa.tipologia;
+                inputid_aliado.value = placa.nombre;
+                inputtelefono.value = placa.celular;
+
+            }else{
+                console.error('Los datos esperados no están presentes en la respuesta de la API');
+            }
+        })  
+    })
+});
