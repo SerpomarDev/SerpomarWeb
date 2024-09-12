6db8d6795c54ff6d5eea2e4304ff7305c72571b0
SerpomarDev-Luifer <luifer.villa@serpomar.com>
2024-09-12 11:16:22 -0500

    fix aliado en preventa

--- js/cargar/cargarPlaca.js
+++ js/cargar/cargarPlaca.js
@@ -59,10 +59,10 @@ document.addEventListener('DOMContentLoaded', function(){
         })
         .then(data =>{
             const placa = data[0]
-            if(placa.eje && placa.tipologia && placa.nombre && placa.celular){
+            if(placa.eje && placa.tipologia && placa.razon_social && placa.celular){
                 inputeje.value = placa.eje;
                 inputtipologia.value = placa.tipologia;
-                inputid_aliado.value = placa.nombre;
+                inputid_aliado.value = placa.razon_social;
                 inputtelefono.value = placa.celular;
 
             }else{
