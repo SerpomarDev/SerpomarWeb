665fffdbc0260adb31fa7be4b7ee7aa112b1082b
Juan Garcia <juangarcia123457854@gmail.com>
2024-10-30 15:29:21 -0500

    fix tipo contenedor

--- js/cargar/cargarTipoServicio.js
+++ js/cargar/cargarTipoServicio.js
@@ -1,21 +1,31 @@
-document.addEventListener('DOMContentLoaded',function(){
+document.addEventListener('DOMContentLoaded', function() {
+
     let selectTipoServicio = document.getElementById('id_tipo_operacion');
-
-    fetch('https://esenttiapp-production.up.railway.app/api/tiposervicios',{
+  
+    fetch('https://esenttiapp-production.up.railway.app/api/tiposervicios', {
         method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${localStorage.getItem("authToken")}`
-            }
-    })
-    .then(Response => Response.json())
-    .then(data=>{
+        headers: {
+          'Authorization': `Bearer ${localStorage.getItem("authToken")}`
+        }
+      })
+      .then(Response => Response.json())
+      .then(data => {
         data.forEach(tipoServ => {
-            let option = document.createElement('option')
-            option.value = tipoServ.id
-            option.text = tipoServ.tipo
-            selectTipoServicio.appendChild(option)       
+          let option = document.createElement('option')
+          option.value = tipoServ.id
+          option.text = tipoServ.tipo
+          selectTipoServicio.appendChild(option)
         });
+      });
 
+    $("#detallesCarga").hide();
+
+    $("#id_tipo_contenedor").change(function() {
+      if ($(this).val() == 10) {
+        $("#detallesCarga").show();
+      } else {
+        $("#detallesCarga").hide();
+      }
     });
-
-});
+  
+  });
