ba1eb97764e3bbd6d93159729262208a9718d44b
SerpomarDev-Luifer <luifer.villa@serpomar.com>
2024-11-22 16:34:13 -0500

    fix enviarCorreo

--- js/resetPassword/enviarCorreo.js
+++ js/resetPassword/enviarCorreo.js
@@ -26,12 +26,10 @@ document.getElementById('envioCorreoReestablecer').addEventListener('submit', fu
           icon: "success",
         });
 
-        setTimeout(() => location.reload(), 2500);
+        setTimeout(() => location.reload(), 1500);
     })
     .catch(error => {
-        console.error('Error al hacer envio del correo:', error);
-
-        Swal.fire({
+            Swal.fire({
             title: "Error",
             text: "Hubo un problema al hacer envio del correo. Por favor, inténtalo de nuevo.",
             icon: "error",
