5db59bd659b06e16656e1a417c04f3e7b6823d3d
SerpomarDev-Luifer <luifer.villa@serpomar.com>
2025-01-08 11:22:52 -0500

    fix: update input element ID for tarifa in cargarRutaClientesV.js

--- js/cargar/cargarRutaClientesV.js
+++ js/cargar/cargarRutaClientesV.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded',function(){
 
     let selectRuta = document.getElementById('id_ruta')
-    let inputTarifa = document.getElementById('tarifa')
+    let inputTarifa = document.getElementById('tarifa1')
 
     fetch('https://esenttiapp-production.up.railway.app/api/uploadRutaclientesv',{
         method: 'GET',
